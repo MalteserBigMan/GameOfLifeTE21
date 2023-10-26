05ab3db6dde524c2c403496fdede12e5353474da
MalteserBigMan <95072327+MalteserBigMan@users.noreply.github.com>
2023-10-26 13:29:21 +0200

    Lagt till Regler och gjort lite förändringar i samtliga filer

--- src/Cell.java
+++ src/Cell.java
@@ -3,7 +3,13 @@ public class Cell {
     int x = 0;
     int y = 0;
 
-    public boolean isAlive(x,y) {
+    public Cell(boolean isAlive, int x, int y) {
+        this.isAlive = isAlive;
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean isAlive(int x, int y) {
         return isAlive;
     }
 
--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,5 +1,5 @@
 public class GameOfLife extends Engine {
-    Engine e;
+
 
     public GameOfLife(int w, int h, int scale)  {
         super(w,h,scale);
